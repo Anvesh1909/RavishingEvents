aa479b5e7e00ce3146bf73a7f4991dac99f910ef
Anvesh190902 <m.anvesh190902@gmail.com>
2024-02-11 21:58:46 +0530

    Deployment

--- Ravishing_events/Ravishing_events/settings.py
+++ Ravishing_events/Ravishing_events/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7mf=9$_g2+#o-0g0bwt2mouei*z=*mzh##zk^a9t#wsigxdr0t
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
